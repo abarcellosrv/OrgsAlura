04edda5a3cd001ebc043afb201dc2e95470177d0
abarcellos <amandabrvalle@gmail.com>
2021-09-28 17:59:44 -0300

    problems with button function

--- app/src/main/java/br/com/alura/orgs/ui/activity/ProductFormActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ProductFormActivity.kt
@@ -2,10 +2,30 @@ package br.com.alura.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import android.widget.Toast.makeText
 import br.com.alura.orgs.R
+import br.com.alura.orgs.model.Products
+import com.google.android.gms.analytics.ecommerce.Product
+import java.math.BigDecimal
 
 class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+
+        val saveButton = findViewById<Button>(R.id.save_button)
+        saveButton.setOnClickListener {
+            val name = findViewById<EditText>(R.id.name).text
+            val nameText = name.toString()
+            val descriptionText = findViewById<EditText>(R.id.description).text
+            val description = descriptionText.toString()
+
+
+        }
     }
 }
