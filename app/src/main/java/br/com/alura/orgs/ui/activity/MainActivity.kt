0e36d0c41e190f9899f1b5d9eb735937b330328e
abarcellos <amandabrvalle@gmail.com>
2021-09-28 15:40:00 -0300

    changed to AppCompat, new activity, changed activity order

--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package br.com.alura.orgs.ui.activity
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
 import br.com.alura.orgs.model.Product
@@ -8,17 +9,10 @@ import br.com.alura.orgs.ui.recyclerview.adapter.ProductListAdapter
 import java.math.BigDecimal
 
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        /*val name = findViewById<TextView>(R.id.name)
-        name.text = "Fruit Salad"
-        val description = findViewById<TextView>(R.id.description)
-        description.text = "Orange, mango and grapes"
-        val price = findViewById<TextView>(R.id.price)
-        price.text = "$19.99"*/
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ProductListAdapter(context = this, products = listOf(
             Product(
