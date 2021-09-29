953ec6a69778f10127b3c53b709c1cf2f0939096
abarcellosrv <amandabrvalle@gmail.com>
2021-09-29 11:05:54 -0300

    commit before merge

--- app/src/main/java/br/com/alura/orgs/ui/activity/ProductFormActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ProductFormActivity.kt
@@ -2,10 +2,17 @@ package br.com.alura.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
+import br.com.alura.orgs.model.Product
+import br.com.alura.orgs.ui.recyclerview.adapter.ProductListAdapter
+import java.math.BigDecimal
 
 class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
+        Log.i("Abracadabra", "HocusPocus")
+
     }
 }
